f2261f61632688513f0a58a624194b36e4ffb465
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2023-05-11 23:38:55 +0200

    adds crawl tests again

--- src/sysfs.rs
+++ src/sysfs.rs
@@ -37,3 +37,50 @@ pub fn crawl(
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use std::io::Write;
+    use std::path;
+    use std::vec;
+    use tempdir;
+
+    #[test]
+    fn test_crawl_simple_file_no_match() {
+        let tmp_dir =
+            tempdir::TempDir::new("myfolder").expect("Could not create a temporary folder");
+        let path = tmp_dir.path().join("myfile.txt");
+        let mut tmp_file = fs::File::create(&path).expect("Could not open a new temp file");
+        writeln!(tmp_file, "Hello").expect("Could not write contents to temp file");
+
+        let mut paths: vec::Vec<path::PathBuf> = vec::Vec::new();
+
+        let re = regex::Regex::new("foo").unwrap();
+        crawl(tmp_dir.path(), &re, &mut paths).expect("Expect crawl to work");
+
+        assert_eq!(paths.len(), 0);
+
+        tmp_dir.close().unwrap();
+    }
+
+    #[test]
+    fn test_crawl_file_match() {
+        let tmp_dir =
+            tempdir::TempDir::new("myfolder").expect("Could not create a temporary folder");
+        let path = tmp_dir.path().join("myfile.txt");
+        let mut tmp_file = fs::File::create(&path).expect("Could not open a new temp file");
+        writeln!(tmp_file, "Hello").expect("Could not write contents to temp file");
+
+        let mut paths: vec::Vec<path::PathBuf> = vec::Vec::new();
+
+        let re = regex::Regex::new("myfile.txt").unwrap();
+        crawl(tmp_dir.path(), &re, &mut paths).expect("Expect crawl to work");
+
+        assert_eq!(paths.len(), 1);
+        assert_eq!(paths[0], path);
+
+        tmp_dir.close().unwrap();
+    }
+}
