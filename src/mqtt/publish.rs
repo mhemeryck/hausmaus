9ca61fe1ac0ac4ab9fa312ce6bfd192c7d2ec1ff
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2023-05-22 22:11:11 +0200

    state topic for device

--- src/mqtt/publish.rs
+++ src/mqtt/publish.rs
@@ -4,8 +4,6 @@ use paho_mqtt;
 
 use crate::sysfs::FileEvent;
 
-const MQTT_TOPIC: &str = "foo";
-
 /// handle_messages receives any file events and sends them out over MQTT
 pub async fn publish_messages(
     mut rx: tokio::sync::mpsc::Receiver<FileEvent>,
@@ -13,11 +11,41 @@ pub async fn publish_messages(
 ) -> paho_mqtt::errors::Result<()> {
     while let Some((device, state, duration)) = rx.recv().await {
         log::debug!("changed: {:?}, {:?}, {:?}", device, state, duration);
-        let message_str = format!("{}|{:?}", state, duration);
-        let message = paho_mqtt::Message::new(MQTT_TOPIC, message_str.as_bytes(), paho_mqtt::QOS_2);
+        let message_str = format!("{} | {} | {:?}", device.name, state, duration);
+        let message = paho_mqtt::Message::new(state_topic_for_device(&device), message_str.as_bytes(), paho_mqtt::QOS_2);
         mqtt_client.publish(message)?;
     }
     Ok(())
 }
 
-//fn state_topic_for_device(device: &crate::device::Device) {}
+/// generate a state topic to publish to for a given device
+fn state_topic_for_device(device: &crate::device::Device) -> String {
+    format!(
+        "{name}/{device_type}/{io_group:1}_{number:02}/state",
+        name = device.name,
+        device_type = match device.device_type {
+            crate::device::DeviceType::DigitalInput => "input",
+            crate::device::DeviceType::DigitalOutput => "output",
+            crate::device::DeviceType::RelayOutput => "relay",
+        },
+        io_group = device.io_group,
+        number = device.number
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_state_topic_for_device() {
+        let device = crate::device::Device {
+            name: String::from("foo"),
+            device_type: crate::device::DeviceType::DigitalOutput,
+            io_group: 1,
+            number: 3,
+        };
+
+        assert_eq!(state_topic_for_device(&device), "foo/output/1_03/state");
+    }
+}
