86964cf7f68f51479d02faa27811133d9e42789a
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2023-06-06 22:40:55 +0200

    [WIP] brainfart

--- src/device.rs
+++ src/device.rs
@@ -1,3 +1,5 @@
+use std;
+
 #[derive(Eq, PartialEq, Debug)]
 pub enum DeviceType {
     DigitalInput,
@@ -11,4 +13,58 @@ pub struct Device {
     pub device_type: DeviceType,
     pub io_group: i32,
     pub number: i32,
+}
+
+/// Represents an Normally Open push button
+struct PushButton {
+    // Link to underlying device
+    device: *const Device,
+    // Current state of the button; either pushed (true) or not (false)
+    state: bool,
+    // Instant to keep track of last update TODO: to be checked how of to push out updates
+    t: std::time::Instant,
+}
+
+/// Represents a light control
+struct Light {
+    // Link to the underlying device
+    device: *const Device,
+    // LIght can be on or off
+    state: bool,
+}
+
+/// Represents a dimmable light control
+struct DimmableLight {
+    device: *const Device,
+    state: bool,
+    // simple 256 level brightness control TODO: to be checked if this is enough (corresponds to
+    // DALI, so probably OK enough
+    brightness: u8,
+}
+
+enum CoverDirection {
+    Up,
+    Down,
+    Stopped,
+}
+
+struct Cover {
+    motor_up: *const Device,
+    motor_down: *const Device,
+    cover_direction: CoverDirection,
+    position: u8,
+}
+
+impl Cover {
+    fn up(&mut self) {
+    }
+
+    fn down(&mut self) {
+    }
+
+    fn go_to_position(&mut self, pos: u8) {
+    }
+
+    fn stop(&mut self) {
+    }
 }
