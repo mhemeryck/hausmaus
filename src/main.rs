aaeb84e17f1e49833b2f78ab9717ecc3edbf60ba
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2023-11-08 22:22:10 +0100

    change file contents on file change

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use crossbeam::channel::{bounded, Sender};
-use serde::{Deserialize, Serialize};
-use serde_yaml;
-use std::collections::HashMap;
-use std::fs::{read_to_string, File};
+// use serde::{Deserialize, Serialize};
+// use serde_yaml;
+// use std::collections::HashMap;
+use std::fs::File;
 use std::io::prelude::*;
 use std::io::Read;
 use std::io::SeekFrom;
@@ -11,10 +11,12 @@ use std::thread::{sleep, spawn};
 use std::time;
 
 const CHANNEL_SIZE: usize = 4;
-const PATH: &str = "/home/mhemeryck/Projects/hausmaus/fixtures/sys/devices/platform/unipi_plc/io_group3/di_3_14/di_value";
+const PATH: &str = "/home/mhemeryck/Projects/hausmaus/fixtures/sys/devices/platform/unipi_plc/io_group3/di_3_14/di_value"; // TODO: as config
+const OUTPUT_PATH : &str = "/home/mhemeryck/Projects/hausmaus/fixtures/sys/devices/platform/unipi_plc/io_group1/do_1_01/do_value"; // TODO: as config
 const DURATION: time::Duration = time::Duration::from_millis(250);
-const CONFIG_FILE: &str = "config.yaml";
+// const CONFIG_FILE: &str = "config.yaml";
 const DEVICE_ID: DeviceId = 1; // TODO: to be determined from crawling file system
+const OUTPUT_DEVICE_ID: DeviceId = 2; // TODO: to be determined from file system
 
 type DeviceId = u64;
 
@@ -25,7 +27,7 @@ type DeviceId = u64;
 //     RelayOutput,
 // }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum State {
     Off,
     On,
@@ -40,38 +42,41 @@ struct FileEvent {
 #[derive(Debug)]
 enum Error {
     FileMonitorErr,
+    FileWriteErr,
 }
 
-#[derive(Debug, Deserialize)]
-struct Config {
-    push_buttons: Vec<PushButton>,
-    lights: Vec<Light>,
-    rules: Vec<Rule>,
-}
+// #[derive(Debug, Deserialize)]
+// struct Config {
+//     push_buttons: Vec<PushButton>,
+//     lights: Vec<Light>,
+//     rules: Vec<Rule>,
+// }
 
-#[derive(Debug, Deserialize)]
-struct PushButton {
-    name: String,
-    device: String,
-}
+// #[derive(Debug, Deserialize)]
+// struct PushButton {
+//     name: String,
+//     device: String,
+// }
 
-#[derive(Debug, Deserialize)]
-struct Light {
-    name: String,
-    device: String,
-}
+// #[derive(Debug, Deserialize)]
+// struct Light {
+//     name: String,
+//     device: String,
+// }
 
-#[derive(Debug, Deserialize)]
-struct Rule {
-    name: String,
-    trigger: String,
-    action: String,
-}
+// #[derive(Debug, Deserialize)]
+// struct Rule {
+//     name: String,
+//     trigger: String,
+//     action: String,
+// }
 
 fn monitor_file(path: &str, sender: Sender<FileEvent>) -> Result<(), Error> {
     let path = Path::new(path);
     let mut file = File::open(path).map_err(|_e| Error::FileMonitorErr)?;
     let mut buf: [u8; 1] = [0; 1];
+
+    let mut prev_state: Option<State> = None;
 
     loop {
         file.read_exact(&mut buf)
@@ -86,48 +91,91 @@ fn monitor_file(path: &str, sender: Sender<FileEvent>) -> Result<(), Error> {
         };
 
         let state = state.ok_or(Error::FileMonitorErr)?;
-        sender
-            .send(FileEvent {
-                device_id: DEVICE_ID,
-                state,
-            })
-            .map_err(|_e| Error::FileMonitorErr)?;
+        match (prev_state, state) {
+            (Some(State::On), State::Off) | (Some(State::Off), State::On) => {
+                sender
+                    .send(FileEvent {
+                        device_id: DEVICE_ID,
+                        state,
+                    })
+                    .map_err(|_e| Error::FileMonitorErr)?;
+            }
+            _ => {}
+        }
+        prev_state = Some(state);
+
         sleep(DURATION);
     }
 }
 
-fn main() {
-    // YAML config
-    let config = read_to_string(CONFIG_FILE).unwrap();
-    let deserialized_config: Config = serde_yaml::from_str(&config).unwrap();
-    println!("Found config {:?}", deserialized_config);
+fn toggle_light(path: &str) -> Result<(), Error> {
+    let mut file = File::open(path).map_err(|_| Error::FileWriteErr)?;
+    let mut buf: [u8; 1] = [0; 1];
 
-    // TODO: crawl for devices
-    let mut config_map = HashMap::new();
-    config_map.insert(DEVICE_ID, "di_3_14");
+    file.read_exact(&mut buf).map_err(|_| Error::FileWriteErr)?;
+
+    let state = match buf[0] as char {
+        '0' => Some(State::Off),
+        '1' => Some(State::On),
+        _ => None,
+    };
+
+    let state = state.ok_or(Error::FileWriteErr)?;
+
+    let mut file = File::create(path).map_err(|_| Error::FileWriteErr)?;
+    match state {
+        State::On => {
+            println!("Convert file from on to off");
+            file.write(b"0").map_err(|_| Error::FileWriteErr)?;
+        }
+        State::Off => {
+            println!("Convert file from off to on");
+            file.write(b"1").map_err(|_| Error::FileWriteErr)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn main() {
+    // // YAML config
+    // let config = read_to_string(CONFIG_FILE).unwrap();
+    // let deserialized_config: Config = serde_yaml::from_str(&config).unwrap();
+    // println!("Found config {:?}", deserialized_config);
+
+    // // TODO: crawl for devices
+    // let mut config_map = HashMap::new();
+    // config_map.insert(DEVICE_ID, "di_3_14");
+
+    let (s, r) = bounded(CHANNEL_SIZE);
+    // let (s1, r1) = bounded(CHANNEL_SIZE);
 
     // File monitor thread
-    let (s, r) = bounded(CHANNEL_SIZE);
-    let (s1, r1) = bounded(CHANNEL_SIZE);
-
     let mut handles = Vec::new();
     let handle = spawn(move || monitor_file(PATH, s));
     handles.push(handle);
+
+    // Main automation thread
     let handle = spawn(move || {
-        while let Ok(e) = r1.recv() {
-            println!("handle from second thread: {:?}", e);
+        while let Ok(e) = r.recv() {
+            println!("{:?}", e);
+            if e.device_id == DEVICE_ID {
+                println!("toggle path: {:?}", e);
+                toggle_light(OUTPUT_PATH).unwrap();
+            }
         }
         Ok(())
     });
     handles.push(handle);
 
-    while let Ok(e) = r.recv() {
-        println!("{:?} - {:?}", e.device_id, e.state);
-        // TODO: rewrap before sending
-        s1.send(e).unwrap();
-    }
+    // while let Ok(e) = r.recv() {
+    //     println!("{:?} - {:?}", e.device_id, e.state);
+    //     // TODO: rewrap before sending
+    //     s1.send(e).unwrap();
+    // }
+
     for handle in handles {
-        handle.join().unwrap();
+        handle.join().unwrap().unwrap();
     }
 
     // handle2.join().unwrap();
